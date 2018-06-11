b7378f4db36d0bb02c3a0aa2936ce2fce1c26c55
Khem Myrick <khem.myrick@gmail.com>
2018-06-11 13:20:06 -0700

    Added most basic functions.
    Search by date, minutes, and search term all working.
    Delete function is working.
    Still to do:
    Complete edit function and tests.py file.

--- work_log2.py
+++ work_log2.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
 """Work Log: Now with Database!
-As of 6:43pm 6/10/18:
-Search name function is just starting to work!
-User can either type a name in, or see a list of available names.
-Planning to approach search date with similar coding.
+As of 12:15pm 6/11/18:
+Search name function seems to work!
+Search task name/notes by search term is working.
+Search date by list is working too!
+Search by date range is working!
+Search by minutes is working!
+Delete function is working!
+
+Next Steps:
+1. Edit function.
+2. Non-peewee tests.
+3. Peewee tests.
+
 
 """
 
@@ -24,10 +33,10 @@ class Entry(Model):
     user_name = CharField(max_length=500)
     task_name = CharField(max_length=1000)
     task_minutes = IntegerField(default=0)
+    # flake8 does NOT like peewee's import convention.
     # I'm not sure if IntegerField takes a "max_length" argument.
     # But, 480 minutes is an 8 hour day, so I figure no task should last longer than 999 minutes.  There's probably an integer-specific alternative to max_length that I can actually set to 480?
     task_notes = TextField()
-
 
     class Meta:
         """Tell entry what database it belongs to?"""
@@ -62,8 +71,9 @@ def current_lists():
         else:
             date_list[str(date_iter)] = str(entry.timestamp.date())
             date_iter += 1
-        
+
     return staff_list, date_list
+
 
 def minute_check(minutes):
     try:
@@ -80,127 +90,56 @@ def delete_entry(entry):
     if input('Delete entry. Are you sure? y/N ').lower() == 'y':
         entry.delete_instance()
         print('Entry deleted.')
+    else:
+        pass
 
 
 def edit_entry(entry):
     """Edit an entry."""
+    # Records can be deleted and edited, letting user change the date, task name, time spent, and/or notes.
+    print("Current task title is " + entry.task_name)
+    new_task = input("Type to update task name, or hit enter to skip.")
+    print('Current notes: \n' + entry.task_notes)
+    new_notes = input("Type to update task notes, or hit enter to skip.")
+    print('Task Minutes: ' + entry.task_minutes)
+    new_minutes = input("Type new integer for minutes, or hit enter to skip.")
+    print('Original Timestamp: ' + entry.timestamp)
+    new_date = input("Type new date MM/DD/YYYY ")
     pass
 
 
-def view_entries(*args):
+def view_entry(search_query=None, search_item=None, fdate=None, ldate=None):
     """View entries."""
-    # This is my attempt to update the work_log.py from TP3.
-    # Seems to be failing miserably for this project.
     entries = Entry.select().order_by(Entry.timestamp.desc())
-    
-    if args[0] == 'user_name':
-        entries = entries.where(Entry.user_name.contains(args[1]))
-    elif args[0] == 'task_name':
-        entries = entries.where(Entry.task_name.contains(args[1]))
-    elif args[0] == 'task_minutes':
-        entries = entries.where(Entry.task_minutes.contains(args[1]))
-    elif args[0] == 'task_notes':
-        entries = entries.where(Entry.task_notes.contains(args[1]))
-    elif args[0] == 'timestamp':
-        entries = entries.where(Entry.timestamp.contains(args[1]))
-    elif args[0] == 'date_range':
-        pass
 
-    e_iter = 0
-    results_loop = 1
-    while results_loop:
-        clear_screen()
-        print(str(len(entries)) + " results match your search.")
-        print("Recorded by: " + entries[e_iter].user_name)
-        print("Date: " + entries[e_iter].timestamp.strftime('%A %B %d'))
-        print("Task: " + entries[e_iter].task_name)
-        print("Time Spent: " + str(entries[e_iter].task_minutes))
-        print("Notes: " + entries[e_iter].task_notes + "\n")
-        print("Entry {} of {}.".format((e_iter + 1), len(entries)))
-
-        if e_iter == 0 and e_iter < (len(results) - 1):
-            review_action = input('''
-[N]ext Item,
-[E]dit Item, [D]elete Item, [R]eturn to Search Menu
-> ''')
-        elif e_iter == (len(results) - 1) and e_iter > 0:
-            review_action = input('''
-[P]revious Item,
-[E]dit Item, [D]elete Item, [R]eturn to Search Menu
-> ''')
-        elif e_iter == (len(results) - 1) and e_iter == 0:
-            review_action = input('''
-[E]dit Item, [D]elete Item, [R]eturn to Search Menu
-> ''')
-        else:
-            review_action = input('''
-[N]ext Item, [P]revious Item,
-[E]dit Item, [D]elete Item, [R]eturn to Search Menu
-> ''')
-
-        if review_action.upper() == "N" and entry < len(results) - 1:
-            entry += 1
-        elif review_action.upper() == "P" and entry > 0:
-            entry -= 1
-        elif review_action.upper() == "E":
-            edit_entry(results[entry])
-            results_loop -= 1
-        elif review_action.upper() == "D":
-            delete_entry(results[entry])
-            results_loop -= 1
-        elif review_action.upper() == "R":
-            results_loop -= 1
-        else:
-            input("Error: Invalid selection. Press enter to try again.")
-
-            # The block above in SQL would look like:
-            # SELECT *FROM entry WHERE user_name LIKE '%search_query%' ORDER BY timestamp DESC
-            # e_iter = 1
-            # for entry in entries:
-            #   clear_screen()
-            #    timestamp = entry.timestamp.strftime('%A %B %d, %Y %I:%M%p')
-            #    print(timestamp)
-            #    print('='*len(timestamp))
-            #    print(entry.user_name)
-            #    print(entry.task_name + ': ' + str(entry.task_minutes))
-            #    print('-' * len(entry.task_name))
-            #    if entry.task_notes:
-            #        print('Notes: ' + entry.task_notes)
-            #    print('\n\n' + '=' * len(timestamp))
-            #    if len(entries) > e_iter:
-            #        print('n) Next entry')
-            #    if e_iter > 1:
-            #        print('p) Previous entry')
-            #    print('d) Delete entry')
-            #    print('q) Return to menu')
-        
-            #    next_action = input('Action: [N/p/d/q]: ').lower().strip()
-            #    if next_action == 'q':
-            #        break
-            #    elif next_action == 'd':
-            #        delete_entry(entry)
-            #    elif next_action == 'n':
-
-
-def view_entry(search_query=None, search_item=None):
-    """View entries."""
-    # From diary.py file from database course.
-    entries = Entry.select().order_by(Entry.timestamp.desc())
-    
     if search_query:
         if search_query == 'user_name':
             entries = entries.where(Entry.user_name.contains(search_item))
-        elif search_query == 'task_name':
-            entries = entries.where(Entry.task_name.contains(search_item))
+        elif search_query == 'task_term':
+            entries = entries.where(
+                Entry.task_name.contains(search_item) |
+                Entry.task_notes.contains(search_item))
         elif search_query == 'date':
-            entries = entries.where(Entry.timestamp.contains(search_query))
+            entries = entries.where(Entry.timestamp.contains(search_item))
+        elif search_query == 'date_range':
+            entries = entries.where(
+                (Entry.timestamp >= fdate) &
+                (Entry.timestamp <= ldate))
         elif search_query == 'task_notes':
-            entries = entries.where(Entry.task_notes.contains(search_query))
+            entries = entries.where(Entry.task_notes.contains(search_item))
         elif search_query == 'task_minutes':
-            entries = entries.where(Entry.task_minutes.contains(search_query))
+            entries = entries.where(
+                (Entry.task_minutes == search_item))
+        elif search_query == 'task_minutes_more':
+            entries = entries.where(
+                (Entry.task_minutes >= search_item))
+        elif search_query == 'task_minutes_less':
+            entries = entries.where(
+                (Entry.task_minutes <= search_item))
+        else:
+            print('Something happened. "search_query" variable not recognized.')
+            input('Press enter to return to menu.')
 
-    # The block above in SQL would look like:
-    # SELECT *FROM entry WHERE thought LIKE '%search_query%' ORDER BY timestamp DESC
     if entries:
         for entry in entries:
             clear_screen()
@@ -215,7 +154,7 @@ def view_entry(search_query=None, search_item=None):
             print('n) Next entry')
             print('d) Delete entry')
             print('q) Return to menu')
-    
+
             next_action = input('Action: [N/d/q]: ').lower().strip()
             if next_action == 'q':
                 break
@@ -228,8 +167,6 @@ def view_entry(search_query=None, search_item=None):
 
 def search_staff():
     """Seach by employee."""
-    # As a user of the script, if finding by employee, I should be allowed to enter employee name and then be presented with entries with that employee as their creator.
-    # If multiple employees share a name (e.g. multiple people with the first name Beth), a list of possible matches is given.
     ss_choice = input('Search by name or see list? [n/L] \n>')
     target = ""
     if ss_choice.lower().strip() == 'n':
@@ -265,7 +202,7 @@ This is what we found. Please select a number.
                     fin = int(fin) - 1
                     if emp_list[fin]:
                         view_entry(search_query="user_name",
-                           search_item=emp_list[fin])
+                                   search_item=emp_list[fin])
                         spec_name -= 1
                         fin_loop -= 1
                     else:
@@ -278,15 +215,15 @@ This is what we found. Please select a number.
                     spec_name -= 1
                 else:
                     continue
-            # entries = Entry.select().order_by(Entry.user_name.desc())
+
     else:
         clear_screen()
-    
+
         name_pick = 1
         while name_pick:
             for key, value in staff_list.items():
                 print(key + ") " + value)
-    
+
             emp_choice = input('Please choose a number from the list.\n> ')
             if emp_choice in staff_list.keys():
                 view_entry(search_query="user_name",
@@ -299,19 +236,94 @@ This is what we found. Please select a number.
 
 def search_date():
     """Search by log date(s)."""
-    pass
+    sd_choice = input('Check specific date range or see list? [r/L] \n>')
+    if sd_choice.lower().strip() == 'r':
+        spec_date = 1
+        while spec_date:
+            clear_screen()
+            target_range = input('''
+Please enter date range in following format:
+"MM/DD/YYYY-MM/DD/YYYY"
+Range will find from midnight of first date, to midnight of the last date.
+So, if you want to search New Year's Day 2018, type "01/01/2018-01/02/2018"
+> ''')
+            targets = target_range.split(sep='-')
+            try:
+                first_date = datetime.datetime.strptime(targets[0],
+                                                        "%m/%d/%Y")
+                last_date = datetime.datetime.strptime(targets[1],
+                                                       "%m/%d/%Y")
+            except ValueError:
+                input('Invalid date range. Press enter to retry.')
+            else:
+                if first_date <= last_date:
+                    spec_date -= 1
+                else:
+                    print('Invalid date range.')
+                    print('Please enter two dates, earliest first.')
+                    input('Press enter to retry.')
+        view_entry(search_query='date_range',
+                   fdate=first_date,
+                   ldate=last_date)
+    else:
+        clear_screen()
+
+        date_pick = 1
+        while date_pick:
+            for key, value in date_list.items():
+                print(key + ") " + value)
+
+            date_choice = input('Please choose a number from the list.\n> ')
+            if date_choice in date_list.keys():
+                view_entry(search_query="date",
+                           search_item=date_list[date_choice])
+                date_pick -= 1
+            else:
+                input('Invalid selection.  Press enter to retry.')
+                clear_screen()
 
 
 def search_minutes():
     """Search by number of task minutes."""
     # As a user of the script, if finding by time spent, I should be allowed to enter the amount of time spent on the project and then be presented with entries containing that amount of time spent.
     # By exact minutes or by range?
-    pass
+    minu_loop = 1
+    while minu_loop:
+        target = ''
+        target = input("Please type a number of minutes.\n> ")
+        if target != '':
+            if re.match(r'\d+', target):
+                # if re.search(r'\-', target):
+                #    view_entry(search_query='task_minutes_less',
+                #               search_item=target)
+                # elif re.search(r'\+', target):
+                #    view_entry(search_query='task_minutes_more',
+                #                search_item=target)
+                view_entry(search_query='task_minutes',
+                           search_item=int(target))
+                    # ValueError: invalid literal for int() with base 10: '%5%'
+                    # '5 was the number i attempted.
+                break
+            else:
+                print('Invalid entry. Please enter only integers.')
+                input('Press enter to retry.')
+        else:
+            print('Invalid entry. Please enter only integers.')
+            input('Press enter to retry.')
 
 
 def search_term():
     """Search by task keyword(s)."""
-    pass
+    # As a user of the script, if finding by a search term, I should be allowed to enter a string and then be presented with entries containing that string in the task name or notes.
+    term_loop = 1
+    while term_loop:
+        target = input('Please type a search term.')
+        if target != '':
+            if re.match(r'\S', target):
+                view_entry(search_query='task_term', search_item=target)
+                break
+        print('Search term may not be blank or begin with a space.')
+        input('Hit enter to retry.')
 
 
 def search_entries():
@@ -367,7 +379,7 @@ def add_entry():
             continue
 
     add_notes = ''
-    if input('Any notes? [y/N]').upper().strip() == "Y": 
+    if input('Any notes? [y/N]').upper().strip() == "Y":
         print('Type notes here. Press "ctrl+d" when finished.\n> ')
         add_notes = sys.stdin.read().strip()
 
@@ -387,17 +399,17 @@ def add_entry():
     print('-' * len(ats_string))
     if input('Save entry? [Y/n]\n> ') != 'n':
         Entry.create(user_name=add_name,
-                    task_name=add_task,
-                    task_minutes=add_minutes,
-                    task_notes=add_notes,
-                    timestamp=add_ts)
+                     task_name=add_task,
+                     task_minutes=add_minutes,
+                     task_notes=add_notes,
+                     timestamp=add_ts)
         print("Saved succesfully!")
 
 
 def main_menu():
     """Show the menu loop."""
     choice = ''
-    
+
     while choice.lower() != 'q':
         clear_screen()
         staff_list, date_list = current_lists()
@@ -412,7 +424,7 @@ def main_menu():
             # .__doc__ will read docstrings of the menu functions.
         print(date_list)
         choice = input("\nAction: ").lower().strip()
-        
+
         if choice in menu:
             menu[choice]()
 
@@ -442,5 +454,5 @@ date_list = OrderedDict([
 
 if __name__ == '__main__':
     initialize()
-    
+
     main_menu()
