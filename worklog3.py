ab6edc1df3ad6117ea21c3d3114f52cb433997a9
Khem Myrick <khem.myrick@gmail.com>
2018-07-02 14:01:38 -0700

    Began new version of worklogdb project.
    Added user interface module, wlux.py
    
    Added worklog3, model and worklog3_test modules.
    
    Worklog3 still needs search functionalitly.
    
    Over 80% of functionality currently in worklog3 is under test.
    
    Less than 20% of statements in wlux are under test.
    
    This all took a very long time.  Hopefully it will all be done this week.

---
+++ worklog3.py
@@ -0,0 +1,162 @@
+"""Work Log DB.
+Return True, False, Item or None.
+Every function returns something.
+Every 'block' is 1 function."""
+
+
+from collections import OrderedDict
+import datetime
+import os
+import re
+import sys
+
+from peewee import *
+from models import Entry
+
+
+# db = SqliteDatabase('log.db')
+
+
+class CardCatalog:
+    """For viewing, sorting and editing worklog database."""
+    title = 'Welcome to the Library of Employee Productivity!'
+    ent_by_name = Entry.select().order_by(Entry.user_name.desc())
+    ent_by_timestamp = Entry.select().order_by(Entry.timestamp.desc())
+    roster = []
+    datelog = []
+    ent_browse = []
+    total_ent = Entry.select().count()
+    curr_ent = 0
+    new_dict = {'user_name': 'John Doe',
+                'task_name': 'Work Task',
+                'task_minutes': 1,
+                'task_notes': ''}
+    # target_entry
+
+    def load_entries(self):
+        # Under test.
+        # Not sure how to test for it NOT working?
+        self.ent_browse = []
+        self.ent_by_timestamp = Entry.select().order_by(Entry.timestamp.desc())
+        for entry in self.ent_by_timestamp:
+            self.new_dict = {'user_name': entry.user_name,
+                             'task_name': entry.task_name,
+                             'task_minutes': entry.task_minutes,
+                             'task_notes': entry.task_notes,
+                             'timestamp': entry.timestamp}
+            self.ent_browse.append(self.new_dict)
+        return self.ent_browse
+
+    def delete_entry(self, entry):
+        """Delete an entry."""
+        # Under test!
+        try:
+            deleter = Entry.get(Entry.user_name==entry['user_name'],
+                                Entry.task_name==entry['task_name'],
+                                Entry.task_minutes==entry['task_minutes'],
+                                Entry.timestamp==entry['timestamp'])
+            deleter.delete_instance()
+            print('Entry deleted.')
+            return True
+        except DoesNotExist:
+            return False
+
+    def generate_blank_entry(self):
+        self.new_dict = {'user_name': 'John Doe',
+                    'task_name': 'Work Task',
+                    'task_minutes': 1,
+                    'task_notes': ''}
+        return self.new_dict
+
+    def generate_roster(self):
+        # This code is returning individual LETTERS rather than strings for some reason??
+        self.ent_by_name = Entry.select().order_by(Entry.user_name.desc())
+        for entry in self.ent_by_name:
+            self.roster += entry.user_name
+        set_list = set(self.roster)
+        self.roster = list(set_list)
+        return self.roster
+
+    def generate_datelog(self):
+        # Like gen_roster, this code is trying to iterate character by character through individual dates
+        # instead of adding each date to the list as intended. 
+        for entry in self.ent_by_timestamp:
+            self.datelog += entry.timestamp.date()
+        set_list = set(self.datelog)
+        self.datelog = list(set_list)
+        return self.datelog
+    
+    def save_new(self, user_name, task_name, task_minutes, task_notes):
+        """Save a new entry."""
+        # Under test.
+        try:
+            Entry.create(user_name=user_name,
+                         task_name=task_name,
+                         task_minutes=task_minutes,
+                         task_notes=task_notes)
+        except ValueError:
+            print('Unexpected values found in entry.')
+            print('Please carefully follow all instructions.')
+            print('Unable to save.')
+            return False
+        else:
+            print('Entry should be added!')
+            return True
+
+    def name_check(self, name):
+        """Check for a string consistent with a first and last name."""
+        # Under test.
+        if re.match(r'(\w+ \w+)', name):
+            return True
+        else:
+            return False
+
+    def minute_check(self, minutes):
+    # Under test.
+        try:
+            int(minutes)
+        except ValueError:
+            return False
+        else:
+            return True
+
+    def notes_out(self, notes):
+        # Under Test.
+        if notes == '':
+            pass
+        elif len(notes) > 40:
+            return 'Notes: ' + notes[:50] + '. . . '
+        else:
+            return 'Notes: ' + notes
+
+    def acquire_target(self, entry):
+        try:
+            output = Entry.get(Entry.user_name==entry['user_name'],
+                               Entry.task_name==entry['task_name'],
+                               Entry.task_minutes==entry['task_minutes'],
+                               Entry.timestamp==entry['timestamp'])
+        except (DoesNotExist, StopIteration):
+            # Under test.  Sort of.
+            # These may not be the correct exceptions?
+            return False
+        else:
+            return output
+
+    def save_edits(self, target_entry):
+        try:
+            # Under Test.
+            target_entry.save()
+            return True
+        except:
+            # Under test.  Sort of.
+            print('Unexpected Error: ', sys.exc_info())
+            print('Cannot confirm save.')
+            return False
+
+#    def start(self):
+#        wlux.main_menu()
+        
+
+# if __name__ == '__main__':
+#    libra = CardCatalog()
+#    libra.start()
---
+++ worklog3_test.py
@@ -0,0 +1,131 @@
+import datetime
+import os
+import re
+import sys
+import unittest
+from unittest.mock import patch, MagicMock
+
+from peewee import *
+from models import Entry
+import wlux
+from worklog3 import CardCatalog
+
+class FunctionTests(unittest.TestCase):
+    def setUp(self):
+        Entry.create(timestamp=datetime.datetime(2018, 7, 2, 8),
+                     user_name='White Diamond',
+                     task_name='Unknown',
+                     task_minutes=1200,
+                     task_notes="mystery")
+
+    def tearDown(self):
+        egg = CardCatalog().delete_entry({'user_name': 'White Diamond',
+                            'task_name': 'Unknown',
+                            'task_minutes': 1200,
+                            'task_notes': "mystery",
+                            'timestamp': datetime.datetime(2018, 7, 2, 8)})
+
+    def test_save_new_good(self):
+        sav = CardCatalog().save_new(
+                 'Steven Universe',
+                 'Containing the Cluster',
+                 11,
+                 "I got you. You got this. We've all got eachother.")
+        self.assertTrue(sav)
+
+    def test_save_new_bad(self):
+        sav = CardCatalog().save_new(
+                 43,
+                 22,
+                 'seventy-eight',
+                 False)
+        self.assertFalse(sav)
+
+    def test_save_edit_good(self):
+        e_dict = {'timestamp': datetime.datetime(2018, 7, 2, 8),
+                  'user_name': 'White Diamond',
+                  'task_name': 'Unknown',
+                  'task_minutes': 1200,
+                  'task_notes': "mystery"}
+        updater = CardCatalog().acquire_target(e_dict)
+        updater.task_name = 'Reknowned'
+        updater.task_minutes = 21
+        updater.task_notes = 'solved'
+        updater.timestamp = datetime.datetime(2015, 6, 2, 8)
+        saver = CardCatalog().save_edits(updater)
+        self.assertTrue(saver)
+
+    @unittest.expectedFailure
+    def test_save_edit_error(self):
+        saver = CardCatalog().save_edits('Not an entry')
+        assertFalse(saver)
+
+    def test_name_check_good(self):
+        self.assertTrue(CardCatalog().name_check('Steven Universe'))
+
+    def test_name_check_bad(self):
+        self.assertFalse(CardCatalog().name_check('Beyonce'))
+
+    def test_minute_check_good(self):
+        self.assertTrue(CardCatalog().minute_check('10'))
+        
+    def test_minute_check_bad(self):
+        self.assertFalse(CardCatalog().minute_check('ten'))
+
+    def test_notes_out_else(self):
+        self.assertEqual(CardCatalog().notes_out('notes'), 'Notes: notes')
+
+    def test_notes_out_empty(self):
+        self.assertIsNone(CardCatalog().notes_out(''))
+
+    def test_notes_out_elif(self):
+        stone = '-' * 50
+        self.assertEqual(CardCatalog().notes_out(stone),
+                         'Notes: ' + stone + '. . . ')
+        
+    def test_load_entries(self):
+        self.assertTrue(CardCatalog().load_entries())
+
+    def test_delete_entry(self):
+        tde = CardCatalog().delete_entry({'user_name': 'White Diamond',
+                            'task_name': 'Unknown',
+                            'task_minutes': 1200,
+                            'task_notes': "mystery",
+                            'timestamp': datetime.datetime(2018, 7, 2, 8)})
+        self.assertTrue(tde)
+
+    def test_delete_entry_dne(self):
+        tde = CardCatalog().delete_entry({'user_name': 'Rose-Quartz',
+                            'task_name': 'Defying Pink Diamond',
+                            'task_minutes': 9999999,
+                            'timestamp': datetime.datetime(2017, 5, 5, 5)})
+        self.assertFalse(tde)
+
+    def test_acquire_target(self):
+        entry_dict = {'timestamp': datetime.datetime(2018, 7, 2, 8),
+                      'user_name': 'White Diamond',
+                      'task_name': 'Unknown',
+                      'task_minutes': 1200,
+                      'task_notes': "mystery"}
+        self.assertEqual(CardCatalog().acquire_target(entry_dict), Entry.get(
+                Entry.user_name=='White Diamond',
+                Entry.task_name=='Unknown',
+                Entry.task_minutes==1200,
+                Entry.timestamp==datetime.datetime(2018, 7, 2, 8)))
+
+    @unittest.expectedFailure
+    def test_acquire_target_dne(self):
+        entry_dict = {'timestamp': datetime.datetime(2000, 1, 1, 1),
+                      'user_name': 'Black Gold',
+                      'task_name': 'Nameless',
+                      'task_minutes': 999999,
+                      'task_notes': "idk"}
+        self.assertFalse(CardCatalog().acquire_target(entry_dict), Entry.get(
+                Entry.user_name=='Black Gold',
+                Entry.task_name=='Nameless',
+                Entry.task_minutes==999999,
+                Entry.timestamp==datetime.datetime(2000, 1, 1, 1)))
+
+
+if __name__ == '__main__':
+    unittest.main()
